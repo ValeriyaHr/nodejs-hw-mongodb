3be02dbefb3b9fa88decc396a8c94c008801b40b
ValeriyaHr <152651276+ValeriyaHr@users.noreply.github.com>
2024-08-29 13:36:02 +0300

    update hm2

---
+++ src/db/initMongoConnection.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+
+import { env } from '../utils/env.js';
+
+export const initMongoDB = async () => {
+  try {
+    const user = env('MONGODB_USER');
+    const password = env('MONGODB_PASSWORD');
+    const url = env('MONGODB_URL');
+    const db = env('MONGODB_DB');
+    const DB_HOST = `mongodb+srv://${user}:${password}@${url}/${db}?retryWrites=true&w=majority`;
+
+// console.log(DB_HOST);
+    await mongoose.connect(DB_HOST);
+    console.log('Mongo connection successfully established!');
+  } catch (error) {
+    console.log('Error while setting up mongo connection', error.message);
+    throw error;
+  }
+};
+
+export default initMongoDB;
+
