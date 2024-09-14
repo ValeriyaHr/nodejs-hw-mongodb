587fb29a2224803b200e97124ea4e6b231363655
ValeriyaHr <152651276+ValeriyaHr@users.noreply.github.com>
2024-09-14 12:06:52 +0300

    upd 4 hw

---
+++ src/middlewares/isValidId.js
@@ -0,0 +1,15 @@
+//src/middlewares/isValidId.js
+
+import { isValidObjectId } from 'mongoose';
+import createHttpError from 'http-errors';
+
+const isValidId = (req, res, next) => {
+  const { contactId } = req.params;
+  if (!isValidObjectId(contactId)) {
+    throw createHttpError(404, 'Not found');
+  }
+
+  next();
+};
+
+export default isValidId;
--- src/routes/contacts.js
+++ src/routes/contacts.js
@@ -1,26 +1,24 @@
-// src/routers/contacts.js
-
 import express from 'express';
 
-import { getContactsController,
+import {
+  getContactsController,
   getContactByIdController,
   createContactController,
   patchContactController,
-  deleteContactController } from "../controllers/contacts.js";
-
-
+  deleteContactController
+} from "../controllers/contacts.js";
 
 import { ctrlWrapper } from "../utils/ctrlWrapper.js";
-import { createContactSchema, updateContactSchema  } from "../validation/contacts.js";
-import { validateBody  } from "../middlewares/validateBody.js";
+import { createContactSchema, updateContactSchema } from "../validation/contacts.js";
+import { validateBody } from "../middlewares/validateBody.js";
+import isValidId from '../middlewares/isValidId.js';
 
 const router = express.Router();
-router.get('/contacts', ctrlWrapper(getContactsController));
-router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
-router.post('/contacts', ctrlWrapper(createContactController));
-router.patch('/contacts/:contactId', ctrlWrapper(patchContactController));
-router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
 
+router.get('/contacts', ctrlWrapper(getContactsController));
+router.get('/contacts/:contactId', isValidId, ctrlWrapper(getContactByIdController));
+router.post('/contacts', validateBody(createContactSchema), ctrlWrapper(createContactController));
+router.patch('/contacts/:contactId', isValidId, validateBody(updateContactSchema), ctrlWrapper(patchContactController));
+router.delete('/contacts/:contactId', isValidId, ctrlWrapper(deleteContactController));
 
 export default router;
-
