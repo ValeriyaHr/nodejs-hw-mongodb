2360da426e9ff2a671f19f684c43d69a1fedd8eb
ValeriyaHr <152651276+ValeriyaHr@users.noreply.github.com>
2024-09-29 23:15:53 +0300

    email50

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -4,7 +4,7 @@ import Joi from 'joi';
 
 export const createContactSchema = Joi.object({
     name: Joi.string().min(3).max(20).required(),
-    phoneNumber: Joi.string().min(3).max(20).required(),
+    phoneNumber: Joi.string().min(3).max(50).required(),
     email: Joi.string().min(3).max(20).email(),
     isFavourite: Joi.boolean(),
     contactType: Joi.string().valid('work', 'home', 'personal'),
@@ -15,7 +15,7 @@ export const createContactSchema = Joi.object({
   export const updateContactSchema = Joi.object({
     name: Joi.string().min(3).max(20),
     phoneNumber: Joi.string().min(3).max(20),
-    email: Joi.string().min(3).max(20).email(),
+    email: Joi.string().min(3).max(50).email(),
     isFavourite: Joi.boolean(),
     contactType: Joi.string().valid('work', 'home', 'personal'),
   });
