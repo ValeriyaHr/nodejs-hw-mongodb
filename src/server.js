2508d86433ffd8dee0128ccc6360afb097f66b4c
ValeriyaHr <152651276+ValeriyaHr@users.noreply.github.com>
2024-10-03 00:20:57 +0300

    update server.js v3

--- src/server.js
+++ src/server.js
@@ -1,5 +1,3 @@
-//server.js
-
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
@@ -23,10 +21,12 @@ const PORT = Number(env('PORT', '3000'));
 export const setupServer = () => {
   const app = express();
 
+  // Middleware для обробки JSON
   app.use(express.json());
   app.use(cors());
   app.use(cookieParser());
 
+  // Логування
   app.use(
     pino({
       transport: {
@@ -35,18 +35,23 @@ export const setupServer = () => {
     }),
   );
 
+  // Додаємо маршрути для аутентифікації та контактів
   app.use('/auth', authRouter);
-
   app.use('/contacts', contactsRouter);
 
+  // Додаємо Swagger документацію
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+  // Middleware для обробки помилок
   app.use(errorHandler);
 
+  // Middleware для обробки 404 помилок
   app.use(notFoundHandler);
 
+  // Запускаємо сервер на визначеному порту
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
 };
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 export default setupServer;
